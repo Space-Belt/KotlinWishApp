393ee6c67fc109c41076963e87b4352e47da364b
Space-Belt <dngurwn@gmail.com>
2025-04-18 15:27:49 +0900

    feat: NavigationIcon View 추가 하기 및 동작 확인

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -3,12 +3,18 @@ package com.example.mywishlistapp
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+
+import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -19,6 +25,17 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit = {}
 ) {
+    val navigationIcon: (@Composable () -> Unit)? =
+        {
+            IconButton (onClick={onBackNavClicked()}) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    tint = Color.White,
+                    contentDescription =  null
+                )
+            }
+    }
+
     TopAppBar (
         title = { Text("title",
             color = colorResource(id = R.color.white),
@@ -27,6 +44,6 @@ fun AppBarView(
         },
         elevation = 3.dp,
         backgroundColor = colorResource(id = R.color.app_bar_color),
-
+        navigationIcon = navigationIcon
     )
 }
