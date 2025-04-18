e66071a7d4a091a3460a582bf2f8460fa870032c
Space-Belt <dngurwn@gmail.com>
2025-04-18 15:14:43 +0900

    feat: Scaffold topBar, res value color에 색추가하기

--- app/src/main/java/com/example/mywishlistapp/MainActivity.kt
+++ app/src/main/java/com/example/mywishlistapp/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyWishListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeView()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyWishListAppTheme {
-        Greeting("Android")
     }
 }
