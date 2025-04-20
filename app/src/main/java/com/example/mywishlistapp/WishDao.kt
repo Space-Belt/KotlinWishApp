b8e9677c973922788a5beb85fe7bd8864c17029c
Space-Belt <dngurwn@gmail.com>
2025-04-20 18:26:08 +0900

    feat: 다오 추상화 추가

---
+++ app/src/main/java/com/example/mywishlistapp/WishDao.kt
@@ -0,0 +1,31 @@
+package com.example.mywishlistapp
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.mywishlistapp.data.Wish
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class WishDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract  fun addWish(wishEntity: Wish)
+
+    @Query("Select * from `wish-table`")
+    abstract  fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract  fun updateAWish(wishEntity: Wish)
+
+    @Delete
+    abstract  fun deleteWish(wishEntity: Wish)
+
+    @Query("Select * from `wish-table` where id=:id")
+    abstract fun getAWishById(): Flow<List<Wish>>
+
+}
