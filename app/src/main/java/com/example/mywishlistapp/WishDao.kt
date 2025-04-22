219abcd09b0612f5aa7fd4dd641cb20f19d654be
Space-Belt <dngurwn@gmail.com>
2025-04-22 18:07:06 +0900

    feat: 안드로이드 manifest WishListApp 추가 & 뷰모델 레포지토리 추가, Dao suspend 추가

--- app/src/main/java/com/example/mywishlistapp/WishDao.kt
+++ app/src/main/java/com/example/mywishlistapp/WishDao.kt
@@ -14,18 +14,18 @@ import kotlinx.coroutines.flow.Flow
 abstract class WishDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    abstract  fun addWish(wishEntity: Wish)
+    abstract suspend fun addAWish(wishEntity: Wish)
 
     @Query("Select * from `wish-table`")
-    abstract  fun getAllWishes(): Flow<List<Wish>>
+    abstract fun getAllWishes(): Flow<List<Wish>>
 
     @Update
-    abstract  fun updateAWish(wishEntity: Wish)
+    abstract suspend fun updateAWish(wishEntity: Wish)
 
     @Delete
-    abstract  fun deleteWish(wishEntity: Wish)
+    abstract suspend fun deleteWish(wishEntity: Wish)
 
     @Query("Select * from `wish-table` where id=:id")
-    abstract fun getAWishById(): Flow<List<Wish>>
+    abstract fun getAWishById(id: Long): Flow<Wish>
 
 }
